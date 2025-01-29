76eb61fc6cefb6fe218ea6278e5439ea4fd1c00f
FluffyTerror <8353207@gmail.com>
2025-01-29 21:20:19 +0300

    partially controllers

--- src/main/java/com/FluffyTerror/Joom2/controller/ProductController.java
+++ src/main/java/com/FluffyTerror/Joom2/controller/ProductController.java
@@ -1,4 +1,15 @@
 package com.FluffyTerror.Joom2.controller;
 
+import com.FluffyTerror.Joom2.service.product.IProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("${api.prefix}/products")
 public class ProductController {
+    private final IProductService productService;
+
+
 }
--- src/main/java/com/FluffyTerror/Joom2/repository/ImageRepository.java
+++ src/main/java/com/FluffyTerror/Joom2/repository/ImageRepository.java
@@ -2,6 +2,8 @@ package com.FluffyTerror.Joom2.repository;
 
 import com.FluffyTerror.Joom2.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ImageRepository extends JpaRepository<Image,Long> {
 }
